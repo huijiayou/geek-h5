1692a407eac7b731705d2f01b01426f2f3abf0ab
huiren <huijiayou139@gmail.com>
2023-01-14 19:40:29 -0800

    add a button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import {Button} from 'antd-mobile-v2'
+
 function App() {
-    return <div>lalal</div>
+    return <div>
+        <Button color='primary'>click</Button>
+    </div>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'
+import 'antd-mobile-v2/dist/antd-mobile.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
